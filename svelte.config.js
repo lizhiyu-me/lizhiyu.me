6cb04f47cec2a84737ac7d18b166a0dcbcdd2f03
SteffanLee <steffan@SteffanLees-MacBook-Air.local>
2023-10-21 02:03:45 +0800

    rm:build dir

--- svelte.config.js
+++ svelte.config.js
@@ -14,10 +14,7 @@ const config = {
 		adapter: adapter(),
 		prerender: {
 			handleHttpError: 'warn'
-		},
-		paths: {
-            base: process.env.NODE_ENV === 'production' ? '/lizhiyu.me' : '',
-        }
+		}
 	},
 	preprocess: [
 		vitePreprocess(),
